1a6d4e71f0bac90e73a8902dcd46f9db78f9555b
zdzulak <43354865+zdzulak@users.noreply.github.com>
2019-12-05 17:00:04 -0500

    Scaffolded api for Albums

---
+++ comp2084-assignment1/Controllers/api/AlbumsController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using comp2084_assignment1.Models;
+
+namespace comp2084_assignment1.Controllers.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumsController : ControllerBase
+    {
+        private readonly zackalbumsContext _context;
+
+        public AlbumsController(zackalbumsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Albums
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Albums>>> GetAlbums()
+        {
+            return await _context.Albums.ToListAsync();
+        }
+
+        // GET: api/Albums/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Albums>> GetAlbums(int id)
+        {
+            var albums = await _context.Albums.FindAsync(id);
+
+            if (albums == null)
+            {
+                return NotFound();
+            }
+
+            return albums;
+        }
+
+        // PUT: api/Albums/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAlbums(int id, Albums albums)
+        {
+            if (id != albums.AlbumId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(albums).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AlbumsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Albums
+        [HttpPost]
+        public async Task<ActionResult<Albums>> PostAlbums(Albums albums)
+        {
+            _context.Albums.Add(albums);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAlbums", new { id = albums.AlbumId }, albums);
+        }
+
+        // DELETE: api/Albums/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Albums>> DeleteAlbums(int id)
+        {
+            var albums = await _context.Albums.FindAsync(id);
+            if (albums == null)
+            {
+                return NotFound();
+            }
+
+            _context.Albums.Remove(albums);
+            await _context.SaveChangesAsync();
+
+            return albums;
+        }
+
+        private bool AlbumsExists(int id)
+        {
+            return _context.Albums.Any(e => e.AlbumId == id);
+        }
+    }
+}
